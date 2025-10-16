d399d55ae1e5dd8bddc02b1a0edce48afa9f0e79
Tarantula1337 <tarantulalp@gmail.com>
2025-10-16 13:01:02 +0200

    Checking for too many lowercase letters added

--- src/scrambler.zig
+++ src/scrambler.zig
@@ -8,10 +8,12 @@ const std = @import("std");
 // -> change patterns
 // -> shift letters using ASCII table
 
-pub fn read_milliseconds() i64 {
+fn read_milliseconds() i64 {
     var now: i64 = 0;
+    // Get timestamp as byte and throw away trash bits
     now = std.time.milliTimestamp();
     now &= 2047;
+    // Normalize milliseconds
     if (now > 1000) {
         if (now > 2000) {
             now = now - 2000;
@@ -22,21 +24,43 @@ pub fn read_milliseconds() i64 {
     return now;
 }
 
-pub fn calc_num_of_pi(password: []u8, milliseconds: i64) i64 {
+fn calc_num_of_pi(password: []u8) i64 {
+    // TODO: build better algorithm to calc number of pi
     var num: i64 = 0;
     for (password) |byte| {
         num += byte;
     }
-    num = num + milliseconds;
+    num = num + read_milliseconds();
     return num;
 }
 
-pub fn correct_password(old_password: u8) u8 {
+fn correct_password(old_password: []u8) []u8 {
+    // Check for invalid characters
+    for (old_password) |i| {
+        if (i == 32) {
+            std.debug.print("ERROR: Password contains invalid character!\n", .{});
+            break;
+        }
+    }
+
+    for (old_password, 0..old_password.len) |_, i| {
+        if (i != 0 and i != old_password.len - 1) {
+            if (97 <= old_password[i - 1] and old_password[i - 1] <= 122) {
+                if (97 <= old_password[i] and old_password[i] <= 122) {
+                    if (97 <= old_password[i + 1] and old_password[i + 1] <= 122) {
+                        old_password[i] = old_password[i] - 32;
+                    }
+                }
+            }
+        }
+    }
+
     return old_password;
 }
 
 pub fn main() !void {
     std.debug.print("Current time: {d}\n", .{read_milliseconds()});
     var password = [_]u8{ 'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd' };
-    std.debug.print("Current password: {d}\n", .{calc_num_of_pi(&password, read_milliseconds())});
+    std.debug.print("Test: {s}\n", .{correct_password(&password)});
+    std.debug.print("Current password: {d}\n", .{calc_num_of_pi(&password)});
 }
